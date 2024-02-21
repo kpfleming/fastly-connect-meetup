4a9a9cc99da333ce03ee63e97dc742e92201140b
Kevin P. Fleming <kevin.fleming@fastly.com>
2024-02-21 16:11:51 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Starter Kit for Fastly Connect Meetup
+
+[![Deploy to Fastly](https://deploy.edgecompute.app/button)](https://deploy.edgecompute.app/deploy)
+
+Simple frontend to a Mardown Converter application running on Glitch,
+which demonstrates the simplicity of using Fastly Compute to
+accelerate existing applications.
+
+**For more details about other starter kits for Compute, see the
+[Fastly developer
+hub](https://developer.fastly.com/solutions/starters)**
---
+++ src/index.js
@@ -0,0 +1,46 @@
+/// <reference types="@fastly/js-compute" />
+
+import MarkdownIt from "markdown-it";
+import { KVStore } from "fastly:kv-store";
+import { SimpleCache } from "fastly:cache";
+
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+
+async function handleRequest(event) {
+    let req = event.request;
+    let url = new URL(req.url);
+    if (req.method == "POST" && url.pathname == "/markdown-to-html") {
+	var md = new MarkdownIt();
+	return new Response(md.render(await req.text()), {
+	    status: 200,
+	    headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
+	});
+    } else if (req.method == "POST" && url.pathname == "/add-item") {
+	const store = new KVStore("demo_kv_store");
+	const receivedText = await req.text();
+	await store.put("items", receivedText);
+	SimpleCache.purge("items", { scope: "pop" });
+	await SimpleCache.getOrSet("items", () => {
+	    return {
+		value: receivedText,
+		ttl: 3600 // Store the entry in the cache for 1 hour
+	    }
+	});
+	return new Response(true, { status: 200 });
+    } else if (req.method == "GET" && url.pathname == "/saved-items") {
+	let result = "";
+	const cache = SimpleCache.get("items");
+	if (cache) {
+	    await cache.text().then((value) => result = value);
+	} else {
+	    const store = new KVStore("demo_kv_store");
+	    const entry = await store.get("items");
+	    result = await entry.text();
+	}
+	return new Response(result);
+    } else {
+	return await fetch(req, {
+	    backend: "demo_backend"
+	});
+    }
+}
